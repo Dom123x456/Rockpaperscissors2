0ccbfb77ee9128e700d5779319d610de9cb6a63c
Dom123x456 <dominicedgar06@gmail.com>
2023-05-14 02:07:02 +0000

    add get resulst and updatescore functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,36 @@ function computerPlay() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+function getResult(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+    } else if (
+        (playerSelection === 'rock' && (computerSelection === 'scissors' || computerSelection === 'lizard')) ||
+        (playerSelection === 'paper' && (computerSelection === 'rock' || computerSelection === 'spock')) ||
+        (playerSelection === 'scissors' && (computerSelection === 'paper' || computerSelection === 'lizard')) ||
+        (playerSelection === 'lizard' && (computerSelection === 'paper' || computerSelection === 'spock')) ||
+        (playerSelection === 'spock' && (computerSelection === 'rock' || computerSelection === 'scissors'))
+    ) {
+        return 'You win!';
+    } else {
+        return 'You lose!';
+    }
+}
+
+function updateScore(result) {
+    if (result === 'You win!') {
+        playerScore++;
+    } else if (result === 'You lose!') {
+        computerScore++;
+    }
+
+    if (playerScore === winningScore) {
+        announceResult('Player wins the game!');
+        disableButtons();
+    } else if (computerScore === winningScore) {
+        announceResult('Computer wins the game!');
+        disableButtons();
+    }
+}
+
